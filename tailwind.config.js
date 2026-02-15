c0d74dc027592a0cc75dce361ab71dd985ae6ba2
bocchimanhwa-web <flexezaur@gmail.com>
2026-02-15 21:33:46 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./index.tsx",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -38,4 +40,4 @@ export default {
     }
   },
   plugins: [],
-}
+} 
